b765cf1709f6ccee1d2e0c65ae21e72fc86479e7
Xeptias <bsegoviasmr1b@gmail.com>
2019-10-15 20:10:31 +0200

    initial

---
+++ AllEqual.java
@@ -0,0 +1,9 @@
+public class AllEqual{
+	
+
+	public static void main(String[] args){
+	int a = Integer.parseInt(args[0]);
+	int b = Integer.parseInt(args[1]);
+	int c = Integer.parseInt(args[2]);
+}
+}
