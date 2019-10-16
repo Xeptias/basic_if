6e8cb2e47b43200437ad930888da2a7406ea8518
Blas_SD <46762615+Xeptias@users.noreply.github.com>
2019-10-16 20:28:59 +0200

    final

--- AllEqual.java
+++ AllEqual.java
@@ -4,7 +4,7 @@ public class AllEqual{
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
 			if (a==b&&b==c){
-				System.out.println("are equals!!");
+				System.out.println("are equals !!");
 			}
 			else System.out.println("Not equals");
 
